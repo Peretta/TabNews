a6ca6cc8620116bcbe9a91c1eff3c86fa0e9d12d
Vinicius <89617179+Peretta@users.noreply.github.com>
2025-01-07 15:24:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1> Oi Mundo! Tudo bem? </h1>
+  return <h1> Oi Mundo! Tudo bem? </h1>;
+}
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identação errada");
 }
 
-export default Home
+export default Home;
